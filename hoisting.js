e5b96c99da8b4515a0329397a3228fe66926a63b
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-02-01 21:11:54 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -8,13 +8,14 @@ function callMe() {
   
 }
 
-
 function crazy() {
-  // fix the code in here:
-   var thisIsCrazy = function (){
-    console.log("hey!!!")
-    
+  var thisIsCrazy = function (){
+    console.log("hey!!!");
   thisIsCrazy()();
+  
+   
+    
+  
  
   }
 }
